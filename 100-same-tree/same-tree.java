aa2b2652fdd9c505600c19fd23aada124dd7d3d3
SHG0202 <53757844+SHG0202@users.noreply.github.com>
2025-08-02 19:02:50 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (69.35%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -15,18 +15,21 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        return checkEqual(p,q);
-    }
-
-    public boolean checkEqual(TreeNode p, TreeNode q){
         if(p != null && q != null){
-            if(checkEqual(p.left, q.left) == false) {return false;}
-            if(p.val != q.val) {return false;}
-            if(checkEqual(p.right, q.right) == false) {return false;} 
-        }else if((p == null && q != null) || (q == null && p != null)){
+            if(p.val != q.val){
+                return false;
+            }
+            if (isSameTree(p.left, q.left) == false){
+                return false;
+            }
+            if(isSameTree(p.right, q.right) == false){
+                return false;
+            }
+        }else if(p == null && q == null){
+            return true;
+        }else{
             return false;
         }
         return true;
     }
-
 }
