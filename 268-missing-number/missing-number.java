c926cafb6b83313cabe967697f8085bf404a4084
SHG0202 <53757844+SHG0202@users.noreply.github.com>
2025-08-31 17:58:59 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (42.77%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,11 +1,22 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int len = nums.length;
-        Arrays.sort(nums);
-        for(int i=0;i<len;i++){
-            if(i != nums[i]) return i;        
-        }
+        // Arrays.sort(nums);
+        // for(int i=0;i<len;i++){
+        //     if(i != nums[i]) return i;        
+        // }
         
-        return len;
+        // return len;
+
+        int xor1 = 0;
+        int xor2 = 0;
+
+        for(int i=0;i<len;i++){
+            xor1 = xor1^nums[i];
+            xor2 = xor2^i;
+        }
+        xor2^=len;
+
+        return xor1^xor2;
     }
 }
