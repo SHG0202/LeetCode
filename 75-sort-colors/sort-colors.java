3a32ea3cf0ae8b4d24e532225f5f65ea797b9f92
SHG0202 <53757844+SHG0202@users.noreply.github.com>
2025-08-31 19:31:35 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (66.64%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,32 +1,55 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int countRed = 0;
-        int countWhite = 0;
-        int countBlue = 0;
+        // int countRed = 0;
+        // int countWhite = 0;
+        // int countBlue = 0;
         int len = nums.length;
 
-        for(int i=0;i<len;i++){
-            if(nums[i] == 0) countRed++;
-            if(nums[i] == 1) countWhite++;
-            if(nums[i] == 2) countBlue++;
-        }
+        // for(int i=0;i<len;i++){
+        //     if(nums[i] == 0) countRed++;
+        //     if(nums[i] == 1) countWhite++;
+        //     if(nums[i] == 2) countBlue++;
+        // }
 
-        for(int i=0;i<len;i++){
-            if(countRed > 0) {
-                nums[i] = 0;
-                countRed--;
-                continue;
-            }
-            if(countWhite > 0) {
-                nums[i] = 1;
-                countWhite--;
-                continue;
-            }
-            if(countBlue > 0){
-                nums[i] = 2;
-                countBlue--;
-                continue;
+        // for(int i=0;i<len;i++){
+        //     if(countRed > 0) {
+        //         nums[i] = 0;
+        //         countRed--;
+        //         continue;
+        //     }
+        //     if(countWhite > 0) {
+        //         nums[i] = 1;
+        //         countWhite--;
+        //         continue;
+        //     }
+        //     if(countBlue > 0){
+        //         nums[i] = 2;
+        //         countBlue--;
+        //         continue;
+        //     }
+        // }
+
+        int low = 0;
+        int mid = 0;
+        int high = len-1;
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums,low, mid);
+                low++;
+                mid++;
+            }else if(nums[mid] == 1){
+                mid++;
+            }else{
+                swap(nums,mid, high);
+                high--;
             }
         }
+    }
+
+    private void swap(int[] nums, int a, int b){
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
 }
