0618e229178692b4060048146ef0d1caf7ed7238
SHG0202 <53757844+SHG0202@users.noreply.github.com>
2025-09-13 00:49:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (73.50%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -12,10 +12,6 @@ class Solution {
         while(low <= high){
             int mid = (low+high)/2;
             
-            // if(mid == 0 && nums[mid] > nums[mid+1]) return mid;
-            // else if(mid == 0) return mid+1;
-            // if(mid == len-1 && nums[mid] > nums[mid-1]) return mid;
-            
             if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid;
             
             if(nums[mid] > nums[mid-1] && nums[mid] < nums[mid+1]) low = mid+1;
