03207ea202b5a4a56ec9648dcc881e2a7b9d7c74
SHG0202 <53757844+SHG0202@users.noreply.github.com>
2025-09-15 20:01:47 +0530

    Time: 9 ms (98.25%) | Memory: 47.1 MB (71.45%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int len = weights.length;
+        int low = 1;
+        int high = 0;
+
+        for(int w : weights){
+            high += w;
+        }
+
+        int result = 0;
+
+        while(low <= high){
+            int mid = (low+high)/2;
+
+            if(isPossible(weights, days, mid)) {
+                high = mid-1;
+                //result = mid;
+            }
+            else low = mid+1;
+        }
+
+        return low;
+    }
+
+    public boolean isPossible(int[] weights, int days, int mid){
+        int tempSum = 0;
+        int tempDays = 0;
+
+        for(int w : weights){
+            if(w > mid) return false;
+            tempSum += w;
+            if(tempSum == mid) {
+                tempDays++;
+                tempSum = 0;
+            }else if(tempSum > mid){
+                tempDays++;
+                tempSum = w;
+            }
+        }
+        
+        if(tempSum > 0) tempDays++;
+
+        if(tempDays <= days) return true;
+        return false;
+    }
+}
