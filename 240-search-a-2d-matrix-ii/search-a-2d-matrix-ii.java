9e0f30676efd4452cba6084dc30fbc7e254962ff
SHG0202 <53757844+SHG0202@users.noreply.github.com>
2025-09-20 20:43:46 +0530

    Time: 5 ms (99.34%) | Memory: 46.2 MB (40.21%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -3,17 +3,26 @@ class Solution {
         int lrow = matrix.length;
         int lcol = matrix[0].length;
 
-        for(int i = 0;i<lrow;i++){
-            int low = 0;
-            int high = lcol-1;
+        // for(int i = 0;i<lrow;i++){
+        //     int low = 0;
+        //     int high = lcol-1;
 
-            while(low <= high) {
-                int mid = (low + high) / 2;
+        //     while(low <= high) {
+        //         int mid = (low + high) / 2;
 
-                if(matrix[i][mid] == target) return true;
-                else if(matrix[i][mid] < target) low = mid + 1;
-                else high = mid - 1;
-            }
+        //         if(matrix[i][mid] == target) return true;
+        //         else if(matrix[i][mid] < target) low = mid + 1;
+        //         else high = mid - 1;
+        //     }
+        // }
+
+        int row = 0;
+        int col = lcol - 1;
+
+        while(row < lrow && col >= 0){
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col] > target) col--;
+            else row++;
         }
 
         return false;
