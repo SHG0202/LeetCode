f2f342d700b67f1d664ead1b3318e50364fbf3d8
SHG0202 <53757844+SHG0202@users.noreply.github.com>
2025-09-22 21:10:36 +0530

    Time: 10 ms (21.02%) | Memory: 45.6 MB (6.58%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -4,12 +4,6 @@ class Solution {
         String reverseWord = "";
 
         for(int i=words.length-1;i>=0;i--){
-            // if(!words[i].equals("")){
-            //     if(reverseWord != ""){
-            //         reverseWord += " ";
-            //     }
-            //     reverseWord += words[i];
-            // }
             reverseWord += words[i];
             if(i != 0){
                 reverseWord += " ";
