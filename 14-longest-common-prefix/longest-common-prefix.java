ba1d26ebecebba396fb14ef14d4d621249cdc45f
SHG0202 <53757844+SHG0202@users.noreply.github.com>
2025-09-22 21:52:59 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (96.17%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,29 +1,51 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if(strs.length == 1){
-            return strs[0];
-        }
+        // if(strs.length == 1){
+        //     return strs[0];
+        // }
 
-        int min_length = strs[0].length();
-        String key = strs[0];
-        for(String s : strs){
-            if(s.length() < min_length){
-                min_length = s.length();
-                key = s;
+        // int min_length = strs[0].length();
+        // String key = strs[0];
+        // for(String s : strs){
+        //     if(s.length() < min_length){
+        //         min_length = s.length();
+        //         key = s;
+        //     }
+        // }
+
+        // int i = 1;
+
+        // while(i <= min_length){
+        //     for (String str : strs){
+        //         if(!str.substring(0,i).equals(key.substring(0,i))){
+        //             return key.substring(0,i-1);
+        //         }
+        //     }
+        //     i++;
+        // }
+        
+        // return key;
+
+        if (strs == null || strs.length == 0) return "";
+
+        // 1. Find the shortest string
+        String minStr = strs[0];
+        for (String s : strs) {
+            if (s.length() < minStr.length()) {
+                minStr = s;
             }
         }
 
-        int i = 1;
-
-        while(i <= min_length){
-            for (String str : strs){
-                if(!str.substring(0,i).equals(key.substring(0,i))){
-                    return key.substring(0,i-1);
+        // 2. Compare only up to minStr.length()
+        for (int i = 0; i < minStr.length(); i++) {
+            char ch = minStr.charAt(i);
+            for (String s : strs) {
+                if (s.charAt(i) != ch) {
+                    return minStr.substring(0, i); // mismatch found
                 }
             }
-            i++;
         }
-        
-        return key;
+
+        return minStr; // whole minStr is a prefix
     }
 }
