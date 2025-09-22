39fc17839a8365ca7b838aed516e4ab8e764368a
SHG0202 <53757844+SHG0202@users.noreply.github.com>
2025-09-22 21:01:29 +0530

    Time: 2 ms (99.06%) | Memory: 42.4 MB (40.14%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,16 +1,34 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        int len = s.length();
-        int open = 1;
-        String result = "";
+        // int len = s.length();
+        // int open = 1;
+        // String result = "";
 
-        for(int i=1;i<len;i++){
-            if(s.charAt(i) == ')') open--;
-            if(s.charAt(i) == '(') open++;
-            if(open == 0 || (open == 1 && s.charAt(i) == '(')) continue;
-            result += String.valueOf(s.charAt(i));
+        // for(int i=1;i<len;i++){
+        //     if(s.charAt(i) == ')') open--;
+        //     if(s.charAt(i) == '(') open++;
+        //     if(open == 0 || (open == 1 && s.charAt(i) == '(')) continue;
+        //     result += String.valueOf(s.charAt(i));
+        // }
+
+        // return result;
+
+        StringBuilder result = new StringBuilder();
+        int depth = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                if (depth > 0) {
+                    result.append(c);
+                }
+                depth++;
+            } else {
+                depth--;
+                if (depth > 0) {
+                    result.append(c);
+                }
+            }
         }
-
-        return result;
+        return result.toString();
     }
 }
