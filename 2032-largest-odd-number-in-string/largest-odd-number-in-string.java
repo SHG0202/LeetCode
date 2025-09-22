d70ea3c1d69eae606aa343b550e344ac0f29f625
SHG0202 <53757844+SHG0202@users.noreply.github.com>
2025-09-22 21:17:16 +0530

    Time: 1 ms (99.48%) | Memory: 45.4 MB (45.08%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
@@ -3,9 +3,10 @@ class Solution {
         int n = num.length();
 
         for(int i=n-1;i>=0;i--){
-            char temp = num.charAt(i);
-            int temp2 = Character.getNumericValue(temp);
-            if(temp2 % 2 == 1){
+            // char temp = num.charAt(i);
+            // int temp2 = Character.getNumericValue(temp);
+            int d = num.charAt(i) - '0';
+            if(d % 2 == 1){
                 return num.substring(0,i+1);
             }
         }
